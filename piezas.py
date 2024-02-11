d5b01397ad64b36066c516f6fd0ed480f6f8851f
SAHIC <elmegabrawler@gmail.com>
2024-02-11 12:03:32 +0100

    actualizacion de can_move

--- piezas.py
+++ piezas.py
@@ -1,15 +1,22 @@
 from enum import StrEnum
-
+from tablero import TableroAjedrez
 class Pieza:
     def __init__(self,color):
         self.color=color
+    def can_move(self,tablero:TableroAjedrez,casilla_inicial,casilla_final):
+        fila, columna=casilla_final
+        if tablero.tablero[fila][columna].color==self.color==self.color:
+            return False
+        return True
+
 
 class Color(StrEnum):
     BLANCO="blanco"
     NEGRO="negro"
 
 class Peon(Pieza):
-    ...
+    def can_move(self,tablero:TableroAjedrez,casilla_inicial,casilla_final):
+        super().can_move(tablero,casilla_inicial,casilla_final)
 
 class Alfil(Pieza):
     ...
